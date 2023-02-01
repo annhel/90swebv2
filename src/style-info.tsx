7fe685e2e2082f55b78366af7dade87da709ec99
Angel <a_martinez00@outlook.com>
2023-02-01 10:35:38 -0500

    fixed some code error

--- src/style-info.tsx
+++ src/style-info.tsx
@@ -18,7 +18,7 @@ export function StyleInfo(prop: StyleProps){
     const noImg = "https://www.google.com/imgres?imgurl=https%3A%2F%2Fcontent.spiceworksstatic.com%2Fservice.community%2Fp%2Fpost_images%2F0000291423%2F5a6917f0%2Fattached_image%2Ffunny-error-messages_preview.jpg&imgrefurl=https%3A%2F%2Fcommunity.spiceworks.com%2Ftopic%2F2105369-10-hilarious-error-messages-facepalm-worthy-computer-prompts-that-make-no-sense&tbnid=2pBXRNX1D45WrM&vet=12ahUKEwjh7se_8fL8AhXVsXIEHcepD78QMygWegUIARDjAQ..i&docid=KFGEULW1xRrdrM&w=400&h=223&q=funny%20error%20messages%20windows&ved=2ahUKEwjh7se_8fL8AhXVsXIEHcepD78QMygWegUIARDjAQ"
 
     return(
-        <div style={divStyle}>
+        <div>
         <h3 style={textStyle}>{name}</h3>
         {/* loading images depending on if link is provided */}
             {image !== ""?<>
